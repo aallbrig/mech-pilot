94b88eaef4eb26f6c722bfa2c9cb7f2fc4339622
Andrew Allbright <aallbrig@gmail.com>
2021-12-17 18:15:53 -0500

    Action is a wrapper around delegates

---
+++ unity/mech-pilot/Assets/Scripts/Core/AI/BehaviorTrees/Behaviors/Action.cs
@@ -0,0 +1,31 @@
+using Core.AI.BehaviorTrees.Behaviors.BuildingBlocks;
+
+namespace Core.AI.BehaviorTrees.Behaviors
+{
+    public class Action: Behavior
+    {
+        public delegate Status ActionCommand();
+        public delegate void ActionSetup();
+        public delegate void ActionTeardown();
+
+        private readonly ActionCommand _action;
+        private readonly ActionSetup _setup;
+        private readonly ActionTeardown _teardown;
+        public Action(ActionCommand action, ActionSetup setup = null, ActionTeardown teardown = null)
+        {
+            _action = action;
+            _setup = setup;
+            _teardown = teardown;
+        }
+
+        public override void Initialize() => _setup?.Invoke();
+
+        public override Status Execute()
+        {
+            CurrentStatus = _action.Invoke();
+            return CurrentStatus;
+        }
+
+        public override void Terminate() => _teardown?.Invoke();
+    }
+}
---
+++ unity/mech-pilot/Assets/Tests/EditMode/Core/AI/BehaviorTrees/Behaviors/ActionUnitTests.cs
@@ -0,0 +1,49 @@
+using Core.AI.BehaviorTrees.Behaviors.BuildingBlocks;
+using NUnit.Framework;
+using Action = Core.AI.BehaviorTrees.Behaviors.Action;
+
+namespace Tests.EditMode.Core.AI.BehaviorTrees.Behaviors
+{
+    public class ActionUnitTests
+    {
+        [Test]
+        public void ActionBehaviors_DeferExecutionDetailsToCaller()
+        {
+            var spyCalled = false;
+            var spyAction = new Action.ActionCommand(() =>
+            {
+                spyCalled = true;
+                return Behavior.Status.Success;
+            });
+            var sut = new Action(spyAction);
+
+            sut.Tick();
+            
+            Assert.IsTrue(spyCalled);
+        }
+
+        [Test]
+        public void ActionBehaviors_OptionallyAllowCustomSetupLogic()
+        {
+            var spyCalled = false;
+            var spySetup = new Action.ActionSetup(() => spyCalled = true);
+            var sut = new Action(() => Behavior.Status.Success, spySetup);
+
+            sut.Tick();
+
+            Assert.IsTrue(spyCalled);
+        }
+        
+        [Test]
+        public void ActionBehaviors_OptionallyAllowCustomTeardownLogic()
+        {
+            var spyCalled = false;
+            var spyTeardown = new Action.ActionTeardown(() => spyCalled = true);
+            var sut = new Action(() => Behavior.Status.Success, null, spyTeardown);
+
+            sut.Tick();
+            
+            Assert.IsTrue(spyCalled);
+        }
+    }
+}
