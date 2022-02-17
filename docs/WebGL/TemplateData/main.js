5a541a5fbb44bb02783051e005fb38133e644c9a
aallbrig <aallbrig@gmail.com>
2022-02-17 20:46:22 +0000

    Deploying to github-pages from @ aallbrig/mech-pilot@20ad16d849c97247729de042bcf7726e3ecd3749 🚀

---
+++ docs/WebGL/TemplateData/main.js
@@ -0,0 +1,14 @@
+var canvas = document.querySelector("#unity-canvas");
+
+var config = {
+    dataUrl: "Build/WebGL.data",
+    frameworkUrl: "Build/WebGL.framework.js",
+    codeUrl: "Build/WebGL.wasm",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "Allbright Web Services LLC",
+    productName: "Mech Pilot",
+    productVersion: "0.0.75",
+    devicePixelRatio: 1,
+}
+
+createUnityInstance(canvas, config);
